e2e90905a3c0c5b07ed7121c077ca56fdc5d9984
+branden <+branden_ott@outlook.com+>
2022-01-25 14:46:07 -0500

    Completed

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  function reverseString(word) {
+    return word.split("").reverse().join("");
+  }
 }
 
 /* 
   Add your pseudocode here
+  create an array from the input string
+reverse the array
+create a string from the reversed array
+return the reversed string
 */
 
 /*
   Add written explanation of your solution here
+  We create an array and m
 */
 
 // You can run `node index.js` to view these console logs
